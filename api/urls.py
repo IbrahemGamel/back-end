c23e1ee393a1152ac86fd0f4f88c70f385d7bb91
IbrahemGamel <ebrahemgamel121@gmail.com>
2024-12-26 18:22:00 +0200

    documentation

--- api/urls.py
+++ api/urls.py
@@ -26,12 +26,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Floewr Garden API",
+      title="Flower Garden API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      contact=openapi.Contact(email="ebrahemgamel121@gmail.com"),
+      license=openapi.License(name="MIT License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
